4d0f1586942bcf379bd01a3806f0358b948b3e71
Mehathab4eva <mehathab4eva@gmail.com>
2017-10-17 18:37:13 -0600

    css path modified

--- homeworkProjects/FairyTail/Assets/game.js
+++ homeworkProjects/FairyTail/Assets/game.js
@@ -43,8 +43,7 @@ var natsuDragneel = {
 	attack: 10,
 	defense: 10,
 	hp: 100,
-	
-	// imageSource: "natsuDragneel.png"
+	imageSource: "./Assets/natsuDragneel.png"
 		};
 
 
@@ -95,15 +94,17 @@ var player3selected = 0; // If player 3 is selected the value become 1.
 
 
 function stats(){
-
+	console.log(hero);
 	$("#heroHeader").text(hero.name);
-	// $("#heroImage").append('<img src= hero.imageSource>'); 
+	$('#heroImage').attr("src", hero.imageSource)
 	$("#heroAttack").text("Attack: "+hero.attack);
 	$("#heroDefense").text("Defense: "+hero.defense);
 	$("#heroHp").text("HP: "+hero.hp);
 	}
 
 function stats2(){
+	console.log(hero);
+	$('#heroImage').attr("src", hero.imageSource)
 	$("#cHeader").text(challenger.name);
 	// $("#cImage").text(challenger.image );
 	$("#cAttack").text("Attack: "+challenger.attack);
@@ -183,11 +184,12 @@ function verdict () {
 // creating the On-Click function to the #natsu
 $("#natsu").on("click",function(){
 
-	
+	console.log(hero);
 	if (player1selected == 0) {
 		mouseClick.play();
 	hero = natsuDragneel; // Natsu becomes hero
 	player1selected ++;
+	$('#heroImage').attr("src", hero.imageSource)
 	// function to update the 1st status div
 	stats();
 	$("#welcome").text("You have selected "+hero.name+". Now please select your opponent.");
@@ -357,7 +359,7 @@ $("#laxus").on("click",function(){
 
 $("#button").on("click", function(){
 	// hero attacks the challenger
-	
+console.log(hero);	
 
 if ((player1selected ==5)|| (gameOver == 1)) {
 	mouseClick.play();
@@ -382,6 +384,19 @@ else if ((player1selected ==2)||(player1selected ==3)||(player1selected ==4)) {
 	}
 	
 });
+
+
+// $("#natsu").hover(function(){
+
+// },
+	// function(){
+	// 	$("natsu").text(natsuDragneel.name/n "Attack: "+ "+ "+natsuDragneel.attack)
+	// });
+
+
+
+
+
 }
 
 
@@ -402,265 +417,5 @@ else if ((player1selected ==2)||(player1selected ==3)||(player1selected ==4)) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.onload = function() // to make sure that JS loads after the webpage is loaded
-// {
-// var audio = new Audio("file:///C:/Users/Mehathab/Desktop/Week3%20Assignment/FairyTail/Assets/FairyTailTheme.mp3");
-// audio.play();
-// var username = prompt("Please enter your name");
-
-// var welcomeNote = document.getElementById("welcome");
-// console.log(welcomeNote);
-// if(welcomeNote ===" "){
-// 	welcomeNote.innerHTML = "Welcome! to the FairyTail Game. Please select your fighter."; 
-// }
-// else{
-// welcomeNote.innerHTML = "Welcome! "+username+"... To the FairyTail Game. Please select your fighter";    
-
-
-// var natsuDragneel = {
-// 	name : "Natsu Dragneel",
-// 	attack: 10,
-// 	defense: 10,
-// 	hp: 100,
-// 	active: 5,
-// 	imageSource: "../Assets/natsuDragneel.png"
-
-// 	};
-
-
-// var erzaScarlet = {
-// 	name : "Erza Scarlet",
-// 	attack: 15,
-// 	defense: 20,
-// 	hp: 100,
-// 	active:6
-	
-// 	};
-
-// var grayFullbuster = {
-// 	name : "Gray Fullbuster",
-// 	attack: 9,
-// 	defense: 9,
-// 	hp: 100,
-// 	active:7
-	
-// 	};
-
-// var laxusDreyar = {
-// 	name : "Laxus Dreyar",
-// 	attack: 19,
-// 	defense: 9,
-// 	hp: 100,
-// 	active:8
-	
-// 	};
-
-// var hero = {};
-// var challenger = {};
-// var steps = 0;
-
-// $("#natsu").on("click",function(){
-// 	if(steps ==0)
-// 		{hero = natsuDragneel;
-// 		stats();
-// 		steps = 1;
-// 		$("#natsu").hide();
-// 		}
-// 	else if ((steps ==1) && (hero !== natsuDragneel)) {
-// 		challenger=natsuDragneel;
-// 		stats2();
-// 		steps2();
-// 		steps = 2;
-// 		$("#natsu").hide();
-
-// 	}
-
-// 	else if ((steps = 3) && (hero != {}) {
-
-// challenger=natsuDragneel;
-// 		stats2();
-// 		steps3();
-// 		steps = 4;
-// 		$("#natsu").hide();	}
-// });
-
-// $("#erza").on("click",function(){
-// 	if(steps ==0) {
-// 	hero = erzaScarlet;
-// 	stats();
-// 	steps = 1;
-// 	$("#erza").hide();
-// }
-// 	else if ((steps ==1) && (hero !== erzaScarlet)) {
-// 		challenger=erzaScarlet;
-// 		stats2();
-// 		$("#erza").hide();
-// 		steps2();
-// 		steps = 2;
-// 		console.log(steps);
-// 	}
-// });
-
-// $("#gray").on("click",function(){
-// 	if(steps ==0) {
-// 	hero = grayFullbuster;
-// 	stats();
-// 	steps = 1;
-// 	$("#gray").hide();
-// 	}
-// 	else if ((steps ==1) && (hero !== grayFullbuster)) {
-// 		challenger=grayFullbuster;
-		
-// 		steps2();
-// 		$("#gray").hide();
-// 		stats2();
-// 		steps = 2;
-// 	}
-// });
-
-// $("#laxus").on("click",function(){
-	
-// 	if(steps ==0) {
-
-// 	hero = laxusDreyar;
-// 	stats();
-// 	steps = 1;
-// 	$("#laxus").hide();
-
-// 	}
-// 	else if ((steps ==1) && (hero !== laxusDreyar)){
-// 		challenger=laxusDreyar;
-		
-// 		steps2();
-// 		$("#laxus").hide();
-// 		stats2();
-// 		steps = 2;
-// 	}
-// 	else{};
-// });
-
-
-
-
-	// function stats(){
-
-	// $("#heroHeader").text(hero.name);
-	// $("#heroImage").append('<img src="../FairyTail/Assets/natsuDragneel.png">'); 
-	// $("#heroAttack").text("Attack: "+hero.attack);
-	// $("#heroDefense").text("Defense: "+hero.defense);
-	// $("#heroHp").text("HP: "+hero.hp);
-	// welcomeNote.innerHTML = "You have selected "+hero.name+". Now please select your opponent."
-	// }
-
-	// function stats2(){
-	// $("#cHeader").text(challenger.name);
-	// $("#cImage").text(challenger.image );
-	// $("#cAttack").text("Attack: "+challenger.attack);
-	// $("#cDefense").text("Defense: "+challenger.defense);
-	// $("#cHp").text("HP: "+challenger.hp);
-	// welcomeNote.innerHTML = "Now that your Fighter and his Opponent are ready. Let the fight begin.";
-	// }
-
-// }
-
-
-// function steps2() {
-	
-// 	$("#displayDiv").css({"position": "absolute", "top": "775px"});
-
-// 	var r = $('<button/>').text("Fight").appendTo("#button");
-// 	r.css({"position":"absolute", "top":"160px","left":"720px","width":"50px", "height":"30px"})
-
-// }
-
-// function steps3() {
-
-// 	$("#button").show();
-
-// }
-
-// $("#button").on("click", function(){
-// 	// hero attacks the challenger
-// if (steps == 2) {
-// 	var hDamage = (hero.attack/challenger.defense)*10;
-// 	var cDamage = (challenger.attack/hero.defense)*10;
-
-// 	var newHp = hero.hp;
-// 	hero.hp = newHp - cDamage;
-// 	newHp = challenger.hp;
-// 	challenger.hp = newHp-hDamage;
-
-// 	console.log("HeroHP: "+hero.hp);
-// 	console.log("OpponentHP: "+challenger.hp);
-// 	stats();
-// 	stats2();
-// 	verdict();
-
-// 	}
-
-// 	else if (steps =3) {
-
-
-// 		document.location.reload(true);
-// 	}
-
-	
-// });
-
-// function verdict () {
-
-	
-
-// 	if ((challenger.hp <=0) && (hero.hp >0)) {
-
-		
-// 		welcomeNote.innerHTML = "Congratulation! That's we call it a victory. Now move on to select you next opponent.";
-// 			steps =3;
-// 		$("#button").hide();
-// 		challenger ={};
-
-
-		
-// 	}
-
-// 	else if((hero.hp <=0) && (challenger.hp >0)){
-		
-// 		steps =3;
-
-// 		welcomeNote.innerHTML = "Your warrior "+hero.name+" has fallen. Sometimes failure are the stepping stones to success. Train you warrior hare next time";
-
-		
-
-
-// 	var a = $('<button/>').text("Refresh").appendTo("#button");
-
-// 	a.css({"position":"absolute", "top":"160px","left":"720px","width":"90px", "height":"30px"})
-
-
-
-
-
-
-
-	
-// 	}}
-
-
-
-// }
 
 
